44b06d83178a5d72470a2abb2800c34568fa40b1
bharat <bharat@192.168.1.222>
2024-09-07 16:55:31 +0530

    Initial commit

---
+++ src/main/java/com/example/kala/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.example.kala;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "You have successfully created Java Webservice KALA using wizard. Awesome right!!!";
+    }
+
+}
+
---
+++ src/test/java/com/example/kala/KalaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
